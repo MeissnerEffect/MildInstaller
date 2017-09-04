1a733893b8c2e1644e90de41443c51c615877799
Mikael GICQUEL <mikael.public@gmail.com>
2017-09-04 22:17:59 +0200

    retrieve_arch_packages: Using CP instead of tar

--- Helpers/999_retrieve_arch_packages_from_repository.sh
+++ Helpers/999_retrieve_arch_packages_from_repository.sh
@@ -1,5 +1,2 @@
-
-
-cd ./../Packages
-docker container export 54ad264bb880 | tar xvf - --wildcards "*.pkg.tar.xz" --strip=4 
-cd -
+#!/bin/bash
+docker cp  kazhed-cemu:/var/cache/pacman/pkg/ ../Packages
