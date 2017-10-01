ecc23f0f806c5aa28d407782360505d06092f6dc
Mikael GICQUEL <mikael.public@gmail.com>
2017-10-01 22:37:13 +0200

    Disabling options not currently tested

--- helpers/cemu.sh
+++ helpers/cemu.sh
@@ -6,7 +6,6 @@ CEMU_HOOK_VERSION=1100c_0541
 CEMU="http://cemu.info/releases/cemu_${CEMU_VERSION}.zip"
 CEMU_HOOK="https://files.sshnuke.net/cemuhook_${CEMU_HOOK_VERSION}.zip"
 
-
 #MESA SETUP
 export mesa_glthread=true
 export force_glsl_extensions_warn=true
@@ -18,53 +17,41 @@ export MESA_VENDOR_OVERRIDE="X.Org"
 export WINEDEBUG="-all"
 export WINEARCH=win64
 export WINEPREFIX="$HOME/.cemu_prefix"
-export WINEDLLOVERRIDES="dbghelp=n" 
+export WINEDLLOVERRIDES="dbghelp=ni,keystone=n" 
 APP=CEMU
 DIRECTORY="$WINEPREFIX/drive_c/cemu"
 EXEC="$DIRECTORY/cemu_latest/Cemu.exe"
-SFLAG="$DIRECTORY/cemu_latest/graphical_setup_done"
 
 function graphical_setup() {
     echo "Running graphical setup for $APP"
     wineboot -u
-    winetricks vcrun2015 corefonts
-    touch $SFLAG
+    winetricks -q vcrun2015 
+    winetricks -q corefonts
  }
 
-function run {
+function run() {
     echo "Run application $APP :  $EXEC"
-[ -f "$SFLAG" ] && wine64 $EXEC || graphical_setup
+    wine64 $EXEC 
 }
- 
 
 function text_setup() {
 
     curl $CEMU > /tmp/cemu.zip
     curl $CEMU_HOOK > /tmp/cemu_hook.zip
-  
-  
     mkdir -p $DIRECTORY
     unzip -qq /tmp/cemu.zip -d $DIRECTORY
-
-  
     rm /tmp/cemu.zip
-    
-  
     cd $DIRECTORY; ln -s cemu_${CEMU_VERSION} cemu_latest
     unzip -qq /tmp/cemu_hook.zip -d $DIRECTORY/cemu_latest
     rm /tmp/cemu_hook.zip
-  
-   
     sed -e "s/#version 420/#version 450/" -i $EXEC ;
     chmod +x $EXEC
-    [ -f "$SFLAG" ] && rm $SFLAG
     exit 0
 }
 
-
 function setup() {
     text_setup
-
+    graphical_setup
 }
 
 [ -f $EXEC ]||setup
